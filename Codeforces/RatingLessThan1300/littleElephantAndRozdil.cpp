b66612fb8aba0b3f1d8f578526d02af0da71076c
padmaja-22 <pskbuggaveeti@gmail.com>
2021-05-28 20:08:46 +0530

    created littleElephatAndRozdil.cpp

---
+++ Codeforces/RatingLessThan1300/littleElephantAndRozdil.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n,d[100000],index,count=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>d[i];
+    }
+    int *m;
+    m=std::min_element(d,d+n);
+    for(int i=0;i<n;i++){
+        if(d[i]==*m){
+            count++;
+            index=i;
+        }
+    }
+    if(count>1){
+        cout<<"Still Rozdil";
+    }
+    else{
+        cout<<index+1;
+    }
+    return 0;
+}
