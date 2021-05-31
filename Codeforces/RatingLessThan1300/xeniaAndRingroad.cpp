88500473fd08a0f198fc2161cdd84c5966412122
padmaja-22 <pskbuggaveeti@gmail.com>
2021-05-31 14:31:47 +0530

    updated

--- Codeforces/RatingLessThan1300/xeniaAndRingroad.cpp
+++ Codeforces/RatingLessThan1300/xeniaAndRingroad.cpp
@@ -2,19 +2,24 @@
 #include <algorithm>
 using namespace std;
 int main(){
-    int n,m,a[100000],sum=0;
+    int n,m,a[100001];
+    long long int sum=0;
     cin>>n>>m;
     for(int i=0;i<m;i++){
         cin>>a[i];
     }
-    sum+=a[0]-1;
+    sum=a[0]-1;
     for(int i=1;i<m;i++){
-        if(a[i]>sum){
-            sum+=(a[i]-sum);
+        if(a[i]>a[i-1]){
+            sum+=(a[i]-a[i-1]);
         }
-        if(a[i]<sum){
-            sum+=(sum-)
+        else if(a[i]<a[i-1]){
+            sum+=((n-a[i-1])+a[i]);
+        }
+        else {
+            sum+=0;
         }
     }
+    cout<<sum;
 
 }
