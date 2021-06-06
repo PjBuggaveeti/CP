7f2921bd3c7c0642c71f0f5116f4220489e24715
padmaja-22 <pskbuggaveeti@gmail.com>
2021-06-06 13:47:00 +0530

    created dubStep.cpp

---
+++ Codeforces/RatingLessThan1300/dubStep.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int flag=1;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B'){
+            i+=2;
+            if(!flag){
+                cout<<" ";
+            }
+            continue;
+        }
+        else{
+            flag=0;
+            cout<<s[i];
+        }
+    }
+    return 0;
+}
+/*
+input : WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB
+output : WE ARE  THE CHAMPIONS MY FRIEND
+*/
