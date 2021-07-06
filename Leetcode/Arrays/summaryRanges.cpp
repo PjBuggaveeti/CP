359b1fc59cb421e06b05b0bde2def3cf2c1876e5
padmaja-22 <pskbuggaveeti@gmail.com>
2021-07-06 13:02:56 +0530

    #228

---
+++ Leetcode/Arrays/summaryRanges.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        if (nums.size()==0){
+            return res;
+        }   
+        for (int i = 0; i < nums.size(); i++) {
+            int a = nums[i]; 
+            while (i < nums.size()-1 && nums[i+1]==nums[i]+1){
+                i++;
+            }
+            if (a != nums[i])
+                res.push_back(to_string(a) + "->" + to_string(nums[i]));
+            else
+                res.push_back(to_string(a) + "");
+        }
+        return res;
+    }
+};
