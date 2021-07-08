3385b134ee865948cae0bb5d7af5fd538e2db3c1
padmaja-22 <pskbuggaveeti@gmail.com>
2021-07-09 00:02:16 +0530

    #414

--- Leetcode/Arrays/thirdMax.cpp
+++ Leetcode/Arrays/thirdMax.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         int n = nums.size();
+        int res;
         sort(nums.begin(),nums.end());
         if(n<3){
-            return nums[n-1];
+            return [n-1];
         }
         int count =1;
         for(int i=n-2;i>=0;i--){
-            if(nums[i+1]!=nums[i])
+            if(nums[i+1]!=nums[i]) //if nums are not same increment count by 1
                 count++;
-            if(count ==3)
-                return nums[i];
+            if(count ==3) //this means we have reached 3rd distinct number from end i.e, 3rd max
+                 return nums[i];
         }
         return nums[n-1];
     }   
